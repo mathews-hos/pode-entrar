cf1c7a142be3f50b54ec8cacfdaad7f58ab2e439
mathews-hos pc paula <mathewshosoares@gmail.com>
2023-06-04 16:20:56 -0300

    yey

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 function theme() {
     let bori = document.body 
     let tema = bori.getAttribute('data-bs-theme')
+    let butao = document.getElementById('portalaluno')
     if (tema =='dark') {
         bori.setAttribute('data-bs-theme', 'light')
+        butao.classList.add('btn-orange')
+        butao.classList.remove('btn-outline-orange')
     } else {
         bori.setAttribute('data-bs-theme', 'dark')
+        butao.classList.add('btn-outline-orange')
+        butao.classList.remove('btn-orange')
+
     }
 }
